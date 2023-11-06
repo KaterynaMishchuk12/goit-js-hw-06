e771430d9668de3bf2d73ec62e73b7384cca5409
KaterynaMishchuk12 <smile.sonrisa7@gmail.com>
2023-11-06 11:16:52 +0200

    all tasks, except 10 are corrected

--- js/task-07.js
+++ js/task-07.js
@@ -3,12 +3,8 @@ const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
 const inputFontSizeHandler = (event) => {
+  text.style.fontSize = Number(input.value);
   text.style.fontSize = event.currentTarget.value + "px";
 };
 
 input.addEventListener("input", inputFontSizeHandler);
-
-// ?? як і куди пов`язати початкове значення шрифту зі значенням інпута
-// пов`язати треба, щоб поверталося початкове положення повзунка і шрифта якщозникає фокус?
-
-// text.style.fontsize = Number(input.value)
